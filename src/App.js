8ec82074449d56baf6f743ba00d1cf5362b7f910
RocksDesireXebec <eengelbertdesire@gmail.com>
2022-07-22 04:19:10 +0200

    Première publication de fluxsell

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Navigate, Route, Routes } from "react-router";
+
+import Navbar from "./components/Navbars/NavbarMain";
+import Footer from "./components/Footer/Footer";
+
+import Landing from "./views/Landing";
+import CategoriesListing from "./views/CategoriesListing";
+import ProductsListing from "./views/ProductsListing";
+import ProductDetails from "./views/ProductDetails";
+import Cart from "./views/Cart";
+import Purchase from "./views/Purchase";
+import Orders from "./views/Orders";
+import NavbarMain from "./components/Navbars/NavbarMain";
+import CategoriesDeCategorie from "./views/CategoriesDeCategorie";
+import PaymentCompletion from "./views/PaymentCompletion";
+import { StateProvider } from "./StateProvider/StateProvider";
+import reducer, { initialState } from "./Reducer/reducer";
+import { Container, Row, Col } from "reactstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid className="App">
+      <StateProvider initialState={initialState} reducer={reducer}>
+        {/* Ici la barre des navigations de gauche sur petit écran <Sidebar />*/}
+        <Row className="main-panel">
+            <NavbarMain />
+            <Routes>
+              <Route path="/accueil" element={<Landing />} />
+              <Route path="/categories" element={<CategoriesListing />} />
+              <Route
+                path="/categories/:id"
+                element={<CategoriesDeCategorie />}
+              />
+              <Route path="/produits/:id" element={<ProductsListing />} />
+              <Route
+                path="/categorie/:idC/produits/:idP"
+                element={<ProductDetails />}
+              />
+              <Route path="/panier" element={<Cart />} />
+              <Route path="/achat/:idProduit" element={<Purchase />} />
+              <Route path="/commandes" element={<Orders />} />
+              <Route
+                path="/paymentcompletion"
+                element={<PaymentCompletion />}
+              />
+              <Route path="*" element={<Landing />} />
+            </Routes>
+            <Footer/>
+        </Row>
+      </StateProvider>
+    </Container>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Route, Routes, } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import "./assets/css/paper-dashboard.css";
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import "./assets/css/navbar.css";
+import "react-multi-carousel/lib/styles.css";
+
+import "./index.css";
+//import "assets/demo/demo.css";
+//import "perfect-scrollbar/css/perfect-scrollbar.css";
+
+import Register from "./views/Register";
+import Login from "./views/Login";
+import NotFound from "./views/NotFound";
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/main/*" element={<App />} />
+        <Route path="/" element={<App />} />
+        <Route path="/register" element={<Register/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
